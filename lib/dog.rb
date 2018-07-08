cbc8287a9af54a8cd355625dad4cc28cf264845f
Steven Crouse <sdcrouse91@gmail.com>
2018-07-08 23:41:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,3 +10,6 @@ end
 # Interesting. This still passes the tests if I define the Person class in this file.
 # It also passes the tests if I define the Dog class in the person file.
 # And it passes the tests if I put both classes in one file and nothing in the other.
+
+roscoe = Dog.new 
+roscoe.sit
